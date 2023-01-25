72f27184833aec5c2b30e33b2a9ae9745c831a95
MATHEUS SANDALO <matheus.sandalo5@gmail.com>
2023-01-24 22:07:55 -0300

    tabelas banco de dados

--- database/migrations/2023_01_25_003659_create_instrucoes_table.php
+++ database/migrations/2023_01_25_003659_create_instrucoes_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('instrucoes', function (Blueprint $table) {
             $table->id();
-            $table->string('nome');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('titulo', 40);
+            $table->text('descricao');
+            $table->string('imagem');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('instrucoes');
     }
 };
